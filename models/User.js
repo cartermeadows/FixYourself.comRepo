f064bd39ebac4de14895562d2afe1695f6ac34e4
Carter_Meadows <cmeado11@charlotte.edu>
2026-03-23 22:40:11 -0400

    Updated User model to include password, roles, and hash

--- models/User.js
+++ models/User.js
@@ -1,20 +1,76 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 50
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 6
+    },
+
+    role: {
+      type: String,
+      enum: ['client', 'coach'],
+      required: true,
+      default: 'client'
+    },
+
+    coach: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true
+  { timestamps: true }
+);
+
+// Hash password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+  } catch (err) {
+    next(err)
   }
-}, {
-  timestamps: true
+});
+
+// Compare entered password to hashed password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+// Remove sensitive fields when sending user data
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
