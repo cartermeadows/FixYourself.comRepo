f45b754b864a5c56dd9ecbc1d8910b6dd0ea4689
Carter_Meadows <cmeado11@charlotte.edu>
2026-03-23 21:56:24 -0400

    Added Middleware and Backend Directories

---
+++ middleware/handleValidationErrors.js
@@ -0,0 +1,15 @@
+const { validationResult } = require("express-validator");
+
+function handleValidationErrors(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array().map(err => err.msg),
+    });
+  }
+
+  next();
+}
+
+module.exports = { handleValidationErrors };
